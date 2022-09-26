26505c2cb83b2550c013d960a6291d7b0fa256c1
David Chaparro <davidacm7@outlook.com>
2022-09-26 11:07:43 -0500

    Round StartNew should StartNewRound

---
+++ src/TheHossGame.Core/RoundAggregate/Round.cs
@@ -0,0 +1,39 @@
+﻿// 🃏 The HossGame 🃏
+// <copyright file="Round.cs" company="Reactive">
+// Copyright (c) Reactive. All rights reserved.
+// </copyright>
+// 🃏 The HossGame 🃏
+
+namespace TheHossGame.Core.RoundAggregate;
+
+using TheHossGame.Core.GameAggregate;
+using TheHossGame.Core.PlayerAggregate;
+using TheHossGame.SharedKernel;
+
+public class Round : EntityBase
+{
+   public Round(GameId gameId, PlayerId firstBidder)
+   {
+      this.GameId = gameId;
+      this.FirstBidder = firstBidder;
+   }
+
+   public GameId GameId { get; }
+
+   public PlayerId FirstBidder { get; }
+
+   public static Round StartNew(GameId gameId, PlayerId playerId)
+   {
+      return new (gameId, playerId);
+   }
+
+   protected override void EnsureValidState()
+   {
+      throw new NotImplementedException();
+   }
+
+   protected override void When(DomainEventBase @event)
+   {
+      throw new NotImplementedException();
+   }
+}
---
+++ tests/TheHossGame.UnitTests/Core/RoundAggregate/Round/StartNewShould.cs
@@ -0,0 +1,30 @@
+﻿// 🃏 The HossGame 🃏
+// <copyright file="StartNewShould.cs" company="Reactive">
+// Copyright (c) Reactive. All rights reserved.
+// </copyright>
+// 🃏 The HossGame 🃏
+
+namespace TheHossGame.UnitTests.Core.RoundAggregate.Round;
+
+using FluentAssertions;
+using TheHossGame.Core.GameAggregate;
+using TheHossGame.Core.PlayerAggregate;
+using TheHossGame.UnitTests.Core.PlayerAggregate;
+using Xunit;
+using Round = TheHossGame.Core.RoundAggregate.Round;
+
+public class StartNewShould
+{
+   [Theory]
+   [AutoPlayerData]
+   public void StartNewRound(
+      AGameId gameId,
+      PlayerId firstBidder)
+   {
+      var round = Round.StartNew(gameId, firstBidder);
+
+      round.Should().NotBeNull();
+      round.GameId.Should().Be(gameId);
+      round.FirstBidder.Should().Be(firstBidder);
+   }
+}
